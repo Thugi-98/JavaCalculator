5c5342f8c2540383bc6c1f38068ce435029e4b0a
Thugi-98 <kimjh4533@gmail.com>
2025-10-20 12:06:10 +0900

    결과값을 저장하는 직접 접근 불가 컬렉션 구현, 컬렉션 값에 접근하는 메서드 (삭제, 가져오기, 수정하기) 구현

--- src/App.java
+++ src/App.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
+
 import method.Calculator;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
 
         // 스캐너 생성
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+
+        Calculator cal = new Calculator();
+
+        int exitCode = 0;
 
         // 반복문 생성
         while (true) {
 
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            int a = scanner.nextInt();
+            int a = scan.nextInt();
 
             System.out.print("두 번째 숫자를 입력하세요: ");
-            int b = scanner.nextInt();
+            int b = scan.nextInt();
 
             System.out.print("사칙연산 기호를 입력하세요: ");
-            char c = scanner.next().charAt(0);
-
-            Calculator cal = new Calculator();
+            char c = scan.next().charAt(0);
 
             // 결과값 출력 전 정상적인 연산인지 검증
             if (c != '+' && c != '-' && c != '*' && c != '/')
@@ -31,11 +34,22 @@ public class Main {
             else
                 System.out.println("결과: " + cal.calculate(a, b, c));
 
-
             // 반복문 종료 여부 확인
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String exit = scanner.next();
-            if (exit.equals("exit")) {
+            String exit = scan.next();
+
+            switch (exit) {
+                case "exit" -> exitCode = 1;
+                case "remove" -> {
+                    System.out.print("삭제 전: ");
+                    cal.getResult();
+                    System.out.print("삭제 후: ");
+                    cal.removeResult();
+                    cal.getResult();
+                }
+            }
+
+            if (exitCode == 1) {
                 break;
             }
 
--- src/method/Calculator.java
+++ src/method/Calculator.java
@@ -1,10 +1,14 @@
 package method;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
     int a;
     int b;
     char c;
+
+    private static ArrayList<Integer> record = new ArrayList<>();
 
     public int calculate(int a, int b, char c) {
 
@@ -12,12 +16,30 @@ public class Calculator {
         this.b = b;
         this.c = c;
 
-        return switch (c) {
-            case '+' -> a + b;
-            case '-' -> a - b;
-            case '*' -> a * b;
-            case '/' -> a / b;
-            default -> 0;
-        };
+        int result;
+
+        switch (c) {
+            case '+' -> result = a + b;
+            case '-' -> result = a - b;
+            case '*' -> result = a * b;
+            case '/' -> result = a / b;
+            default -> result = 0; /* 사칙연산 기호가 아닐 경우에도 반환되는 값은 존재해야 함 */
+        }
+
+        record.add(result);
+
+        return result;
+    }
+
+    public void removeResult() {
+        record.remove(0);
+    }
+
+    public void getResult() {
+        System.out.println(record);
+    }
+
+    public void setResult() {
+        record.add(0);
     }
 }
