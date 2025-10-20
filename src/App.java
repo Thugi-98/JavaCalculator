1accf5e62da28c6d5e7f555a0c338a271823cb6b
Thugi-98 <kimjh4533@gmail.com>
2025-10-20 13:07:21 +0900

    결과값을 저장하는 직접 접근 불가 컬렉션 구현, 컬렉션 값에 접근하는 메서드 (삭제, 가져오기, 수정하기) 구현

--- src/App.java
+++ src/App.java
@@ -1,18 +1,19 @@
-import java.util.Scanner;
-
 import method.Calculator;
+
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-        // 스캐너 생성
-        Scanner scan = new Scanner(System.in);
-
+        /* Calculator 인스턴스 생성 */
         Calculator cal = new Calculator();
+
+        /* 스캐너 생성 */
+        Scanner scan = new Scanner(System.in);
 
         int exitCode = 0;
 
-        // 반복문 생성
+        /* 반복문 시작 */
         while (true) {
 
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -24,21 +25,22 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char c = scan.next().charAt(0);
 
-            // 결과값 출력 전 정상적인 연산인지 검증
+            /* 결과값 출력 전 정상적인 연산인지 검증 */
             if (c != '+' && c != '-' && c != '*' && c != '/')
                 System.out.println("잘못된 사칙연산 기호입니다.");
             else if (b == 0 && c == '/')
                 System.out.println("0으로는 나눌 수 없습니다.");
 
-            // 정상적인 연산일 경우 결과 출력
-            else
+            /* 정상적인 연산일 경우 결과 출력 */
+            else {
                 System.out.println("결과: " + cal.calculate(a, b, c));
+                cal.setResult(cal.calculate(a, b, c));
+            }
 
-            // 반복문 종료 여부 확인
+            /* 반복문 종료 여부 확인 */
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String exit = scan.next();
-
-            switch (exit) {
+            String command = scan.next();
+            switch (command) {
                 case "exit" -> exitCode = 1;
                 case "remove" -> {
                     System.out.print("삭제 전: ");
@@ -49,9 +51,8 @@ public class App {
                 }
             }
 
-            if (exitCode == 1) {
+            if (exitCode == 1)
                 break;
-            }
 
         }
     }
